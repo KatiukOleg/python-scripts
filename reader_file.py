d9069c169cfedbd93012131a13ae213e28aa6352
KatiukOleg <katiukoleg@gmail.com>
2026-01-01 21:23:57 +0200

    add read_data def

--- reader_file.py
+++ reader_file.py
@@ -1,9 +1,11 @@
+import os
 import sys
 import argparse
+import json
 from rich.console import Console
 
 
-def analyze_text(file_path):
+def analyze_txt(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         lines = file.readlines()
 
@@ -16,11 +18,19 @@ def analyze_text(file_path):
         "content": cleaned_lines
     }
 
+def read_data(file_path):
+    ext = os.path.splitext(file_path)[1]
+    if ext == ".txt":
+        return analyze_txt(file_path)
+    if ext == ".json":
+        return analyze_json(file_path)
+    raise ValueError("Unsupported format")
+
 if __name__ == "__main__":
     console = Console()
     parser = argparse.ArgumentParser(description="Simple script for reading files")
     parser.add_argument("--file", "-f", required=True, help="Name of file")
     arguments = parser.parse_args()
-    result = analyze_text(arguments.file)
+    result = read_data(arguments.file)
     console.print("Python version:" + sys.version)
     console.print(result)
